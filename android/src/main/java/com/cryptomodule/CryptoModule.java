cd78083bc053f60e25a76d6f02a589f9115d6f06
Navid Ahmad <naveedkhan028@gmail.com>
2025-07-18 18:35:27 +0800

    change namespcae

--- android/src/main/java/com/cryptomodule/CryptoModule.java
+++ android/src/main/java/com/cryptomodule/CryptoModule.java
@@ -9,7 +9,7 @@ import com.facebook.react.bridge.Promise;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
--- android/src/main/java/com/cryptomodule/CryptoModulePackage.java
+++ android/src/main/java/com/cryptomodule/CryptoModulePackage.java
@@ -1,10 +1,10 @@
-package main.java.com.cryptomodule;
+package com.cryptomodule;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-
+import main.java.com.cryptomodule.CryptoModule;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -12,7 +12,7 @@ import java.util.List;
 public class CryptoModulePackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new CryptoModule(reactContext));
+        return Arrays.<NativeModule>asList(new Cry(reactContext));
     }
 
     @Override
