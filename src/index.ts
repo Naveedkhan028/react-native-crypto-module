6def146df6c762136b3f356772eb93c44b99ece4
Navid Ahmad <naveedkhan028@gmail.com>
2025-07-30 06:16:01 +0800

    Fixed native encrypt streaming, encrypt/decrypt text

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,29 @@ interface CryptoModuleInterface {
     keyBase64: string,
     ivBase64: string
   ): Promise<string>;
+  
+  decryptTextContent(
+    encryptedContentBase64: string,
+    keyBase64: string,
+    ivBase64: string
+  ): Promise<string>;
+  
+  encryptTextContent(
+    textContent: string,
+    keyBase64: string,
+    ivBase64: string
+  ): Promise<string>;
+  
+  encryptDataStreaming(
+    inputDataBase64: string,
+    keyBase64: string,
+    ivBase64: string,
+    chunkSize: number
+  ): Promise<{
+    encryptedChunks: string[];
+    totalChunks: number;
+    totalProcessed: number;
+  }>;
 }
 
 const CryptoModule: CryptoModuleInterface = NativeModules.CryptoModule;
